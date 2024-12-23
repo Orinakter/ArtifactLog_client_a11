95b5708e2579fe188f58fc1e762d361bc540c222
Orinakter <orin.akter9013@gmail.com>
2024-12-23 23:33:41 +0600

    see all button created successfully

---
+++ src/Components/SeeAllButton.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SeeAllButton = () => {
+    return (
+        <div className='text-center mt-24'>
+            <button className='btn bg-blue-500 text-white font-bold text-2xl '>See All Button</button>
+        </div>
+    );
+};
+
+export default SeeAllButton;
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -4,6 +4,7 @@ import HistorySpotlight from '../Components/HistorySpotlight';
 import LegacyOfCulture from '../Components/LegacyOfCulture';
 import FeaturedArtifacts from '../Components/FeaturedArtifacts';
 import { useLoaderData } from 'react-router-dom';
+import SeeAllButton from '../Components/SeeAllButton';
 
 const HomePage = () => {
     const allData = useLoaderData()
@@ -12,6 +13,7 @@ const HomePage = () => {
         <div>
            <Banner></Banner>
            <FeaturedArtifacts allData={allData}></FeaturedArtifacts>
+           <SeeAllButton></SeeAllButton>
            <LegacyOfCulture></LegacyOfCulture>
            <HistorySpotlight></HistorySpotlight>
         </div>
