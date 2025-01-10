b4ceb346a6dc5bbb3942c2a295a9b4b14549fe44
Orinakter <orin.akter9013@gmail.com>
2025-01-11 02:19:42 +0600

    footer updated done

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FaFacebook, FaFacebookSquare, FaInstagram, FaTwitter } from 'react-icons/fa';
+import { FaFacebook, FaFacebookSquare, FaInstagram, FaLinkedin, FaTwitter } from 'react-icons/fa';
 import { FaLocationDot } from 'react-icons/fa6';
 import { IoIosContact } from 'react-icons/io';
 import { MdEmail } from 'react-icons/md';
@@ -10,7 +10,10 @@ const Footer = () => {
   <div className="container mx-auto px-4 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 gap-6">
    
     <div>
-      <h2 className="text-xl font-bold text-white mb-4">Historical Artifacts Tracker</h2>
+     <div className="flex items-center gap-3 mb-4">
+      <img src="https://i.ibb.co.com/ggyxXy7/360-F-104185226-IKo-Yj7o-Cdg-Ier8bbn-Kg-Idw-IOSxsgg-TXo-1.jpg" alt=""  className="w-[50px] h-[40px] rounded-full" />
+     <h2 className="text-xl font-bold text-white">ArtifactLog</h2>
+     </div>
       <p className="text-sm ">
         Discover and preserve history with our user-friendly tracker. Explore, add, and learn about historical artifacts from around the world.
       </p>
@@ -33,7 +36,7 @@ const Footer = () => {
       <ul className="space-y-2">
         <li className="flex items-center gap-3">
         <MdEmail />
-          <a className="hover:text-blue-500">info@artifactstracker.com</a>
+          <a className="hover:text-blue-500">info@ArtifactLog.com</a>
         </li>
         <li class="flex items-center gap-3">
         <IoIosContact />
@@ -50,14 +53,14 @@ const Footer = () => {
       <h2 className='text-lg font-bold mb-2'>Follow Us</h2>
       <div className="flex space-x-4 text-xl">
 
-      <p><FaFacebook/></p>
+      <a href='https://www.facebook.com/orinakter.2000?mibextid=ZbWKwL'><FaFacebook/></a>
             
-            <p>
-              <FaTwitter />
-            </p>
-            <p>
+            <a href='https://www.linkedin.com/in/orinakter23/'>
+            <FaLinkedin />
+            </a>
+            <a href='https://www.instagram.com/orin_akter123?igsh=MXR4Z3Q0ZGJrdm1zeQ=='>
               <FaInstagram />
-            </p>
+            </a>
 
       </div>
     </div>
@@ -65,7 +68,7 @@ const Footer = () => {
 
   
   <div className="mt-8 border-t border-gray-700 pt-6 text-center text-sm">
-    <p>Copyright 2024 Historical Artifacts Tracker. All rights reserved.</p>
+    <p>Copyright 2024 ArtifactLog. All rights reserved.</p>
     <p>
       Developed by <a className="text-blue-500 hover:underline">Orin Akther</a>.
     </p>
